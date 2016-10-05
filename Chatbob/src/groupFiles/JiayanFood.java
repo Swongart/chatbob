1970235c9a68123a7b51e5ba8f0dadc568385ba5
Student 6 <Student 6@10.8.33.178>
2016-10-05 12:21:39 -0400

    basic methods

--- Chatbob/src/groupFiles/JiayanFood.java
+++ Chatbob/src/groupFiles/JiayanFood.java
@@ -3,15 +3,42 @@ package groupFiles;
 public class JiayanFood implements Topic {
 private boolean inFoodLoop;
 private String foodResponse;
+//private static String [] foods = {"pizza","ice cream","Japanese Food", "Korean Food"};
+private static String [] restaurants = {"Di Fara Pizza","Ample Hills Creamery","Kyo Ya","Shilla"};
 
 	public void talk() {
 	inFoodLoop = true;
 	while(inFoodLoop)
 	{
-		
+		foodResponse = SharonChatBox.getInput();
+		suggestRestaurants();
+		if(!isTriggered(foodResponse))
+		{
+			inFoodLoop = false;
+			SharonChatBox.talkForever();
+		}
 	}
 		 
 
+	}
+	private void suggestRestaurants()
+	{
+		if(SharonChatBox.findKeyword(foodResponse, "pizza", 0)>= 0)
+		{
+			SharonChatBox.print("Since you like pizza, I suggest you go to "+ restaurants[0]);
+		}
+		if(SharonChatBox.findKeyword(foodResponse, "ice cream", 0)>= 0)
+		{
+			SharonChatBox.print("In New York, I think "+ restaurants[1] + "is great for ice cream");
+		}
+		if(SharonChatBox.findKeyword(foodResponse, "Japanese Food", 0)>= 0)
+		{
+			SharonChatBox.print("I know all about Japanese food! The "+ restaurants[2]+ "restaurant is awesome!");
+		}
+		if(SharonChatBox.findKeyword(foodResponse, "Korean Food", 0)>= 0)
+		{
+			SharonChatBox.print("In manhattan, I suggest you go to "+ restaurants[3] + "for authentic korean food.");
+		}
 	}
 	public boolean isTriggered(String userInput) {
 		if(SharonChatBox.findKeyword(userInput, "food", 0)>= 0)
@@ -22,6 +49,7 @@ private String foodResponse;
 		{
 			return true;
 		}
+		
 		return false;
 	}
 
