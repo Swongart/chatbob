dc521a4cb59f71185ff9cf58281d925d299446bf
Student6 <Student6@10.8.33.176>
2016-10-06 12:32:50 -0400

    small bug

--- Chatbob/src/groupFiles/AnnaWWE.java
+++ Chatbob/src/groupFiles/AnnaWWE.java
@@ -6,7 +6,6 @@ public class AnnaWWE implements Topic{
 
 	private boolean inWWELoop;
 	private String WWEResponse;
-	private String wrestler;
 	
 	@Override 
 	public void talk() {
@@ -15,13 +14,18 @@ public class AnnaWWE implements Topic{
 		inWWELoop = true;
 		while (inWWELoop){
 			WWEResponse = SharonChatBox.getInput();
-			int likePsn = SharonChatBox.findKeyword(WWEResponse,"yes",0);
-			if(likePsn>=0){
+			if(SharonChatBox.findKeyword(WWEResponse,"yes",0)>=0){
 				SharonChatBox.print("Do you like WWE?");
+			
 				if (SharonChatBox.findKeyword(WWEResponse, "yes", 0)>=0){
 					SharonChatBox.print("You are so cool. Who do you like?");
-					 String wrestler = WWEResponse;
-					 SharonChatBox.print(wrestler + " is the best!");
+					String[] divas = {"dean ambrose", "seth rollins", "roman reigns", "aj styles", "kevin owens","chris jericho", "charlotte", "sasha banks", "becky lynch", "bayley", "john cena", "randy orton"};
+					for ( int i=0; i<divas.length; i++){
+						if (SharonChatBox.findKeyword(WWEResponse,divas[i],0)>=0){
+							SharonChatBox.print(divas[i] + " is the best!");
+						}
+					}
+					
 				}
 				else{
 					inWWELoop = false;
